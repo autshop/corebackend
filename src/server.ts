f924deba9275bef22a31071cf55293f68e475a2b
Akos Fi <fi.akos@straxus.hu>
2021-04-16 17:51:33 +0200

    [feature] Base project.

---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import dotenv from "dotenv";
+dotenv.config();
+//
+import app from "./app";
+//import sequelize from "db";
+
+const port = process.env.PORT || 3000;
+
+(async () => {
+    try {
+        //await sequelize.authenticate();
+        await app.listen(port);
+        console.log(`Listening on port: ${port}.`);
+    } catch (err) {
+        console.log("Failed to start application.");
+        console.log("Reason: ", err);
+    }
+})();
